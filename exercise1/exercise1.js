df1c4251534a64e38f5e8715b5c4072998a4a445
marshingjay <jam605@gmail.com>
2018-09-21 14:44:28 -0500

    finished implementing exercise 1

--- exercise1/exercise1.js
+++ exercise1/exercise1.js
@@ -5,7 +5,7 @@ function validate(){
     var pw1 = document.getElementById("password1");
     var pw2 = document.getElementById("password2");
     var result = document.getElementById("resultMessage");
-    if(pw1.length < 8 || pw2.lenth < 8){
+    if(pw1.value.length < 8 || pw2.value.length < 8){
         result.innerHTML = "Error: password must be at least 8 characters long!";
     }
     else if(pw1.value === pw2.value){
